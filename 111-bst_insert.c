2cc4f6f53bba024ae1b8f72ee100f5461f8e5007
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-01 10:46:05 -0700

    inserts a value in a Binary Search Tree

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,62 +1,46 @@
 #include "binary_trees.h"
 /**
- * bst_insert_recursion - recursively traverses a Binary Search Tree to insert
- * a new value
- *
- * @tree: binary tree node
- * @value: integer to insert in new node
- * Return: pointer to new node, or NULL on failure or value found already
- * found in tree
- */
-bst_t *bst_insert_recursion(bst_t *tree, int value)
-{
-	bst_t *new = NULL;
-
-	if (tree->n > value)
-	{
-		if (tree->left == NULL)
-		{
-			new = binary_tree_node(tree, value);
-			tree->left = new;
-			return (new);
-		}
-
-		return (bst_insert_recursion(tree->left, value));
-	}
-
-	if (tree->n < value)
-	{
-		if (tree->right == NULL)
-		{
-			new = binary_tree_node(tree, value);
-			tree->right = new;
-			return (new);
-		}
-
-		return (bst_insert_recursion(tree->right, value));
-	}
-
-	return (NULL);
-}
-
-
-/**
- * bst_insert - inserts a value in a Binary Search Tree
- * @tree: binary tree node passed by reference
- * @value: integer to insert in new node
- * Return: pointer to new node, or NULL on failure or value found already
- * found in tree
+ * bst_insert - insert nodes in order to create a BST tree
+ * @tree: tree to create with type BST
+ * @value: value of node to insert
+ * Return: BST tree
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *new = NULL;
-
-	if (!tree || !(*tree))
-	{
-		new = binary_tree_node(NULL, value);
-		*tree = new;
-		return (new);
-	}
-
-	return (bst_insert_recursion(*tree, value));
+bst_t *new, *trv;
+binary_tree_t *aux;
+if (!tree)
+	return (NULL);
+if (*tree == NULL)
+{
+aux = binary_tree_node((binary_tree_t *)(*tree), value);
+new = (bst_t *)aux;
+*tree = new;
+}
+else
+{
+trv = *tree;
+if (value < trv->n)
+{
+if (trv->left)
+	new = bst_insert(&trv->left, value);
+else
+{
+aux = binary_tree_node((binary_tree_t *)trv, value);
+new = trv->left = (bst_t *)aux;
+}
+}
+else if (value > trv->n)
+{
+if (trv->right)
+	new = bst_insert(&trv->right, value);
+else
+{
+aux = binary_tree_node((binary_tree_t *)trv, value);
+new = trv->right = aux;
+}
+}
+return (NULL);
+}
+return (new);
 }
